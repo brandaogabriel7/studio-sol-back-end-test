425f0bd9a528bcd3361b84b8c5344451bca83759
Gabriel Brandão <gabriel.silva.brandao7@gmail.com>
2022-12-19 12:30:13 -0300

    test: add integration tests for minLowercase and minUppercase

--- src/integration_tests/verify_test.go
+++ src/integration_tests/verify_test.go
@@ -51,23 +51,58 @@ var _ = Describe("Verify", func() {
 			Entry("Test case 1", "Opa1@", 5, 1, 1),
 			Entry("Test case 2", "SenhaForte!23", 5, 1, 1),
 		)
-	})
 
-	Context("Checking that password does not follow all the specified rules", func ()  {
-		DescribeTable("The following rules fail:",
-			func (password string, minSize int, minSpecialChars int, minDigit int, noMatch []string)  {
+		DescribeTable("minUppercase, minLowercase, minDigit",
+			func (password string, minUppercase int, minLowercase int, minDigit int)  {
 				var resp struct {
 					Verify model.Verify
 				}
 				
 				error := c.Post(
 					`
-					query($password: String!, $minSize: Int!, $minSpecialChars: Int!, $minDigit: Int!) {
+					query($password: String!, $minUppercase: Int!, $minLowercase: Int!, $minDigit: Int!) {
+						verify(password: $password, rules: [
+							{rule: "minUppercase", value: $minUppercase},
+							{rule: "minLowercase", value: $minLowercase},
+							{rule: "minDigit", value: $minDigit},
+						]) {
+							verify
+							noMatch
+						}
+					}`,
+					&resp,
+					client.Var("password", password),
+					client.Var("minUppercase", minUppercase),
+					client.Var("minLowercase", minLowercase),
+					client.Var("minDigit", minDigit),
+				)
+	
+				Expect(error).NotTo(HaveOccurred())
+				Expect(resp.Verify.Verify).To(BeTrue())
+				Expect(resp.Verify.NoMatch).To(BeEmpty())
+			},
+			Entry("Test case 1", "OOOOoPpppa1@", 3, 5, 1),
+			Entry("Test case 2", "S3nHaForTe!23", 4, 4, 3),
+		)
+	})
+
+	Context("Checking that password does not follow all the specified rules", func ()  {
+		DescribeTable("The following rules fail:",
+			func (password string, minSize int, minSpecialChars int, minDigit int, minUppercase int, minLowercase int, noMatch []string)  {
+				var resp struct {
+					Verify model.Verify
+				}
+				
+				error := c.Post(
+					`
+					query($password: String!, $minSize: Int!, $minSpecialChars: Int!, $minDigit: Int!,$minUppercase: Int!, $minLowercase: Int!) {
 						verify(password: $password, rules: [
 							{rule: "minSize", value: $minSize},
 							{rule: "minSpecialChars", value: $minSpecialChars},
 							{rule: "noRepeted", value: 0},
 							{rule: "minDigit", value: $minDigit},
+							{rule: "minUppercase", value: $minUppercase},
+							{rule: "minLowercase", value: $minLowercase},
 						]) {
 							verify
 							noMatch
@@ -78,6 +113,8 @@ var _ = Describe("Verify", func() {
 					client.Var("minSize", minSize),
 					client.Var("minSpecialChars", minSpecialChars),
 					client.Var("minDigit", minDigit),
+					client.Var("minUppercase", minUppercase),
+					client.Var("minLowercase", minLowercase),
 				)
 	
 				Expect(error).NotTo(HaveOccurred())
@@ -85,9 +122,10 @@ var _ = Describe("Verify", func() {
 				Expect(resp.Verify.NoMatch).NotTo(BeEmpty())
 				Expect(resp.Verify.NoMatch).To(Equal(noMatch))
 			},
-			Entry("minSize", "0p@", 5, 1, 1, []string{string(validation.MIN_SIZE)}),
-			Entry("minSize, minSpecialChars, minDigit", "SenhaForte!2", 20, 4, 2, []string{string(validation.MIN_SIZE), string(validation.MIN_SPECIAL_CHARS), string(validation.MIN_DIGIT)}),
-			Entry("noRepeted", "aaaaaaa!2", 3, 1, 1, []string{string(validation.NO_REPETED)}),
+			Entry("minSize", "O0p@", 5, 1, 1, 1, 1, []string{string(validation.MIN_SIZE)}),
+			Entry("minSize, minSpecialChars, minDigit", "SenhaForte!2", 20, 4, 2, 1, 1, []string{string(validation.MIN_SIZE), string(validation.MIN_SPECIAL_CHARS), string(validation.MIN_DIGIT)}),
+			Entry("noRepeted, minUppercase", "aaaaaaa!2", 3, 1, 1, 3, 1, []string{string(validation.NO_REPETED), string(validation.MIN_UPPERCASE)}),
+			Entry("minUppercase, minLowercase", "senha!2", 3, 1, 1, 3, 9, []string{string(validation.MIN_UPPERCASE), string(validation.MIN_LOWERCASE)}),
 		)
 	})
 })
