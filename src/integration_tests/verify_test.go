1aa156c2a3c6106d553f316cdd216975142ddaab
Gabriel Brandão <gabriel.silva.brandao7@gmail.com>
2022-12-18 21:10:18 -0300

    test: add failure integration tests

--- src/integration_tests/verify_test.go
+++ src/integration_tests/verify_test.go
@@ -12,36 +12,76 @@ import (
 var _ = Describe("Verify", func() {
 	c := client.New(handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}})))
 	
-	DescribeTable("Checking that password follows minSize, minSpecialChars, noRepeted, minDigit",
-		func (password string, minSize int, minSpecialChars int, minDigit int)  {
-			var resp struct {
-				Verify model.Verify
-			}
-			
-			error := c.Post(
-				`
-				query($password: String!, $minSize: Int!, $minSpecialChars: Int!, $minDigit: Int!) {
-					verify(password: $password, rules: [
-						{rule: "minSize", value: $minSize},
-						{rule: "minSpecialChars", value: $minSpecialChars},
-						{rule: "noRepeted", value: 0},
-						{rule: "minDigit", value: $minDigit},
-					]) {
-						verify
-						noMatch
-					}
-				}`,
-				&resp,
-				client.Var("password", password),
-				client.Var("minSize", minSize),
-				client.Var("minSpecialChars", minSpecialChars),
-				client.Var("minDigit", minDigit),
-			)
+	Describe("Checking that password follows the specified rules", func ()  {
+		DescribeTable("minSize, minSpecialChars, noRepeted, minDigit",
+			func (password string, minSize int, minSpecialChars int, minDigit int)  {
+				var resp struct {
+					Verify model.Verify
+				}
+				
+				error := c.Post(
+					`
+					query($password: String!, $minSize: Int!, $minSpecialChars: Int!, $minDigit: Int!) {
+						verify(password: $password, rules: [
+							{rule: "minSize", value: $minSize},
+							{rule: "minSpecialChars", value: $minSpecialChars},
+							{rule: "noRepeted", value: 0},
+							{rule: "minDigit", value: $minDigit},
+						]) {
+							verify
+							noMatch
+						}
+					}`,
+					&resp,
+					client.Var("password", password),
+					client.Var("minSize", minSize),
+					client.Var("minSpecialChars", minSpecialChars),
+					client.Var("minDigit", minDigit),
+				)
+	
+				Expect(error).NotTo(HaveOccurred())
+				Expect(resp.Verify.Verify).To(BeTrue())
+				Expect(resp.Verify.NoMatch).To(BeEmpty())
+			},
+			Entry("Test case 1", "Opa1@", 5, 1, 1),
+			Entry("Test case 2", "SenhaForte!23", 5, 1, 1),
+		)
+	})
 
-			Expect(error).NotTo(HaveOccurred())
-			Expect(resp.Verify.Verify).To(BeTrue())
-		},
-		Entry("Test case 1", "Opa1@", 5, 1, 1),
-		Entry("Test case 2", "SenhaForte!23", 5, 1, 1),
-	)
+	Describe("Checking that password does not follow all the specified rules", func ()  {
+		DescribeTable("The following rules fail:",
+			func (password string, minSize int, minSpecialChars int, minDigit int, noMatch []string)  {
+				var resp struct {
+					Verify model.Verify
+				}
+				
+				error := c.Post(
+					`
+					query($password: String!, $minSize: Int!, $minSpecialChars: Int!, $minDigit: Int!) {
+						verify(password: $password, rules: [
+							{rule: "minSize", value: $minSize},
+							{rule: "minSpecialChars", value: $minSpecialChars},
+							{rule: "noRepeted", value: 0},
+							{rule: "minDigit", value: $minDigit},
+						]) {
+							verify
+							noMatch
+						}
+					}`,
+					&resp,
+					client.Var("password", password),
+					client.Var("minSize", minSize),
+					client.Var("minSpecialChars", minSpecialChars),
+					client.Var("minDigit", minDigit),
+				)
+	
+				Expect(error).NotTo(HaveOccurred())
+				Expect(resp.Verify.Verify).To(BeFalse())
+				Expect(resp.Verify.NoMatch).NotTo(BeEmpty())
+				Expect(resp.Verify.NoMatch).To(Equal(noMatch))
+			},
+			Entry("minSize", "0p@", 5, 1, 1, []string{"minSize"}),
+			Entry("minSize, minDigits, minSpecialChars", "SenhaForte!23", 20, 4, 2, []string{"minSize", "minDigits, minSpecialChars"}),
+		)
+	})
 })
